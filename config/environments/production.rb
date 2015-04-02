72ac971316f64b9a60f26a39cdb8797296c2dffd
monika <monikaglier@gmail.com>
2015-04-02 13:46:05 +0200

    removed password from development.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,19 @@ Odot::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  #config.action_mailer.default_url_options = { :host => 'localhost:3000'}
+  #config.action_mailer.default_url_options = { :host => 'smtp.sendgrid.net'}
+  ActionMailer::Base.raise_delivery_errors = true
+  ActionMailer::Base.perform_deliveries = true 
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
 
 end
