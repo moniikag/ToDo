efb7caa3c8e2b30b253e40d20b09cb3737dcba00
monika <monikaglier@gmail.com>
2015-05-10 17:21:47 +0200

    changed factories to sequences

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,18 +1,18 @@
 FactoryGirl.define do
-  factory :user do
-    email "factory@example.com"
-    password "password"
-    password_confirmation "password"
+  factory :user do |u|
+    u.sequence(:email) { |n| "#{n}factory@example.com" }
+    u.sequence(:password) { |n| "#{n}password" }
+    u.sequence(:password_confirmation) { |n| "#{n}password" }
   end
 
-  factory :todo_list do
-    title "FactoryTodoList "
-    description "FactoryTodoListDescription "
+  factory :todo_list do |l|
+    l.sequence(:title) { |n| "Factory#{n}TodoList " }
+    l.sequence(:description) { |n| "Factory#{n}TodoListDescription " }
     user
   end
 
-  factory :todo_item do
-    content "Todo Item"
+  factory :todo_item do |i|
+    i.sequence(:content) { |n| "#{n} Todo Item" }
     deadline "2015-05-30 10:00:00"
     todo_list
   end
