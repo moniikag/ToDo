83e1f9c4171132ea0bfc74053f058b0afc1a8535
monika <monikaglier@gmail.com>
2015-05-13 12:23:30 +0200

    fixed: display only todo_items that belong to todo_list

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -2,7 +2,7 @@ class TodoItemsController < ApplicationController
   before_action :get_resources
   
   def index
-    @todo_items = policy_scope(TodoItem)
+    @todo_items = policy_scope(TodoItem).where(todo_list: @todo_list)
   end
 
   def new
@@ -50,7 +50,7 @@ class TodoItemsController < ApplicationController
   private
   def get_resources
     @todo_list = policy_scope(TodoList).find(params[:todo_list_id])
-    @todo_item = policy_scope(TodoItem).find(params[:id]) if params[:id]
+    @todo_item = policy_scope(TodoItem).where(todo_list: @todo_list).find(params[:id]) if params[:id]
     if @todo_item
       authorize @todo_item
     else
