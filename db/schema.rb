5923b1efdf33b857ded7d854856f4cb4d9f6dd06
monika <monikaglier@gmail.com>
2015-06-15 11:35:16 +0200

    removed first_name, last_name from users; todo_item content htmlvalidation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150528094726) do
+ActiveRecord::Schema.define(version: 20150615092859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,8 +68,6 @@ ActiveRecord::Schema.define(version: 20150528094726) do
   add_index "todo_lists", ["user_id"], name: "index_todo_lists_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
-    t.string   "first_name"
-    t.string   "last_name"
     t.string   "email"
     t.string   "password_digest"
     t.datetime "created_at"
